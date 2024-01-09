81889d33b801119e87f86ff06de470fe0c184f8f
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2024-01-09 22:35:58 +0300

    09.01.2024

---
+++ BlogProject/ViewComponents/BoxWidget/BlogTitleSlider.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.ViewComponents.BoxWidget
+{
+    public class BlogTitleSlider : ViewComponent
+    {
+        BlogManager BlogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            List<Blog> blogList = BlogManager.GetList(bl => bl.ObjectStatus == 1)
+                .OrderByDescending(bl => bl.ObjectIDate)
+                .Take(3)
+                .ToList();
+            return View(blogList);
+        }
+    }
+}
