58bbeef5a2154be9e3e2c1d577fc4691225bde29
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2024-01-09 21:45:42 +0300

    09.01.2024

--- BlogProject/ViewComponents/BoxWidget/EditorsChoiceBoxWidget.cs
+++ BlogProject/ViewComponents/BoxWidget/EditorsChoiceBoxWidget.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.ViewComponents.BoxWidget
 {
     public class EditorsChoiceBoxWidget : ViewComponent
     {
+        BlogManager BlogManager = new BlogManager(new EfBlogRepository());
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var baseUri = $"{Request.Scheme}://{Request.Host}/";
+            List<Blog> blogList = new List<Blog>();
+            blogList = BlogManager.GetBlogListWithCategory(bl => bl.ObjectStatus == 1)
+                .OrderByDescending(x => x.ObjectId)
+                .Take(3)
+                .ToList();
+            foreach (var item in blogList)
+            {
+                item.ThumbnailImage = item.ThumbnailImage.Replace(@"\", @"/");
+                item.ThumbnailImage = baseUri + item.ThumbnailImage;
+                item.MainImage = baseUri + item.MainImage;
+            }
+
+            return View(blogList);
         }
     }
 }
