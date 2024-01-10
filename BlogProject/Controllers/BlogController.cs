b88d68299939e94885250737daaf2fa867dbf8d1
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2024-01-10 23:50:39 +0300

    10.01.2023

--- BlogProject/Controllers/BlogController.cs
+++ BlogProject/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
 ﻿using BlogProject.DTO;
+using BlogProject.ViewModels;
 using BusinessLayer.Concrete;
 using DataAccesLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Reflection.Metadata;
 
 namespace BlogProject.Controllers
@@ -11,9 +13,46 @@ namespace BlogProject.Controllers
     {
         BlogManager BlogManager = new BlogManager(new EfBlogRepository());
         BlogTagManager BlogTagManager = new BlogTagManager(new EfBlogTagRepository());
+        CategoryManager CategoryManager = new CategoryManager(new EfCategoryRepository());
+        TagManager TagManager = new TagManager(new EfTagRepository());
 
+        private FilterBlogListViewModel FilterBlogListViewModel()
+        {
+            List<Category> Category = CategoryManager.GetList();
+            List<Tag> Tag = TagManager.GetList();
 
+            List<SelectListItem> CategoryItem = new List<SelectListItem>();
+            List<SelectListItem> TagItem = new List<SelectListItem>();
+
+            foreach (var item in Category)
+            {
+                SelectListItem selectListItem = new SelectListItem();
+                selectListItem.Text = item.Name;
+                selectListItem.Value = item.ObjectId.ToString();
+                CategoryItem.Add(selectListItem);
+            }
+
+            foreach (var item in Tag)
+            {
+                SelectListItem selectListItem = new SelectListItem();
+                selectListItem.Text = item.Name;
+                selectListItem.Value = item.ObjectId.ToString();
+                TagItem.Add(selectListItem);
+            }
+
+            FilterBlogListViewModel filterBlogListViewModel = new FilterBlogListViewModel(CategoryItem, TagItem);
+            return filterBlogListViewModel;
+        }
+
+        [HttpGet]
         public IActionResult Index()
+        {
+            FilterBlogListViewModel filterBlogListViewModel = FilterBlogListViewModel();
+            return View(filterBlogListViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string? parameters)
         {
 
             return View();
---
+++ BlogProject/ViewModels/FilterBlogListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogProject.ViewModels
+{
+    public class FilterBlogListViewModel
+    {
+        public FilterBlogListViewModel(List<SelectListItem> categoryItem, List<SelectListItem> tagItem, List<Blog> blogList)
+        {
+            CategoryItem = categoryItem;
+            TagItem = tagItem;
+            Blogs = blogList;
+        }
+
+        public FilterBlogListViewModel(List<SelectListItem> categoryItem, List<SelectListItem> tagItem)
+        {
+            CategoryItem = categoryItem;
+            TagItem = tagItem;
+        }
+
+        public string Title { get; set; }
+        public int CategryId { get; set; }
+        public int TagId { get; set; }
+        public int OrderId { get; set; }
+
+        public List<SelectListItem> CategoryItem { get; set; }
+        public List<SelectListItem> TagItem { get; set; }
+        public List<Blog> Blogs { get; set; }
+
+    }
+}
