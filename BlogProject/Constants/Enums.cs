0324e24f2de5d808acc4a9dd27b11e74481c8e19
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2024-01-11 20:05:58 +0300

    Enum sınıfı eklendi

---
+++ BlogProject/Constants/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace BlogProject.Constants
+{
+    public static class Enums
+    {
+        public  enum ObjectStatus
+        {
+            Active = 1,
+            Passive= 0
+        }
+
+        public enum Order
+        {
+            MostRead = 1,
+            MostCommented = 2,
+            NewestUploadDate = 3,
+            OldestUploadDate = 4,
+        }
+    }
+}
