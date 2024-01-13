250b0f524cc0104c100798d272555a6838f32860
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2024-01-13 23:16:36 +0300

    13.01.2024

---
+++ BlogProject/Controllers/CommentController.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddComment(Comment comment )
+        {
+            return RedirectToAction("Blog");
+        }
+    }
+}
